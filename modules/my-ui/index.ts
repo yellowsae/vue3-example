c79f4772da9708bf1a764b090d755e16fe6b5b11
yellowsea <yunhai0644@163.com>
2022-12-07 12:27:36 +0800

    feat(vue-components): 实现组件库的的按需加载
    - 全局加载
    
    BREAKING CHANGE: 按需&全局加载
    
    #0

--- modules/my-ui/index.ts
+++ modules/my-ui/index.ts
@@ -1,5 +1,6 @@
 import Select from "./Select/index.vue"
 import Link from "./Link/index.vue"
+//  
 
 const COMPONENTS = [
   Select,
@@ -7,20 +8,20 @@ const COMPONENTS = [
 ]
 
 // 按需加载 
-export const MySelect = Vue => Vue.component("MySelect", Select)
-export const MyLink = Vue => Vue.component("MyLink", Link)
+export const MySelect = (Vue: any) => { Vue.component("MySelect", Select) }
+export const MyLink = (Vue: any) => { Vue.component("MyLink", Link) }
 
 // 全局加载 
 const MyUI: any = {}
 
-MyUI.install = function (Vue) {
+MyUI.install = function (Vue: any) {
   // Vue.component("MySelect", MySelect)
   // Vue.component("MyLink", MyLink)
 
   COMPONENTS.forEach((component) => {
     Vue.component(component.name, component)
-    Vue.component("MySelect", MySelect)
-    Vue.component("MyLink", MyLink)
+    Vue.component("MySelect", Select)
+    Vue.component("MyLink", Link)
   })
 }
 
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,10 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import { MyLink, MySelect } from "../modules/my-ui"
-// import MyUI from "../modules/my-ui"
+import MyUI from "../modules/my-ui"
 const app = createApp(App)
 // app.use(MyUI)
-// app.use(MyLink)
-// app.use(MySelect)
+app.use(MyLink)
+app.use(MySelect)
 
 app.mount('#app')
